b9982f623d38921d37affb17c813cbd16b9ec9d2
Masato Odakura <aerospatium@gmail.com>
2017-03-17 17:03:31 +0900

    [Add] 音を可視化するプログラム群<visualizer>追加
    AudioPlot：音の波形を表示
    SpectrumAnalyzer：音のスペクトルを表示
    SoundSpring：音のスペクトルを色に変換

---
+++ visualizer/AudioPlot.py
@@ -0,0 +1,60 @@
+# -*- coding:utf-8 -*-
+
+#プロット関係のライブラリ
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+import numpy as np
+import sys
+
+#音声関係のライブラリ
+import pyaudio
+import struct
+
+
+class PlotWindow:
+    def __init__(self):
+        #プロット初期設定
+        self.win=pg.GraphicsWindow()
+        self.win.setWindowTitle(u"リアルタイムプロット")
+        self.plt=self.win.addPlot() #プロットのビジュアル関係
+        self.plt.setYRange(-0.05,0.05)    #y軸の上限、下限の設定
+        self.curve=self.plt.plot()  #プロットデータを入れる場所
+
+        #マイクインプット設定
+        self.CHUNK=64             #1度に読み取る音声のデータ幅
+        self.RATE=1000             #サンプリング周波数
+        self.audio=pyaudio.PyAudio()
+        self.stream=self.audio.open(format=pyaudio.paInt16,
+                                    channels=1,
+                                    rate=self.RATE,
+                                    input=True,
+                                    frames_per_buffer=self.CHUNK)
+
+        #アップデート時間設定
+        self.timer=QtCore.QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(50)    #10msごとにupdateを呼び出し
+
+        #音声データの格納場所(プロットデータ)
+        self.data=np.zeros(self.CHUNK)
+
+    def update(self):
+        self.data=np.append(self.data, self.AudioInput())
+        if len(self.data)/64 > 5:     #5*1024点を超えたら1024点を吐き出し
+            self.data=self.data[64:]
+        self.curve.setData(self.data)   #プロットデータを格納
+   
+
+    
+    def AudioInput(self):
+        ret=self.stream.read(self.CHUNK)    #音声の読み取り(バイナリ)
+        #バイナリ → 数値(int16)に変換
+        #32768.0=2^16で割ってるのは正規化(絶対値を1以下にすること)
+        ret=np.frombuffer(ret, dtype="int16")/32768.0
+        return ret
+
+if __name__=="__main__":
+    plotwin=PlotWindow()
+    if (sys.flags.interactive!=1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
+
---
+++ visualizer/SpectrumAnalyzer.py
@@ -0,0 +1,68 @@
+# -*- coding:utf-8 -*-
+
+#プロット関係のライブラリ
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+import numpy as np
+import sys
+
+#音声関係のライブラリ
+import pyaudio
+import struct
+
+
+class PlotWindow:
+    def __init__(self):
+        #マイクインプット設定
+        self.CHUNK=1024            #1度に読み取る音声のデータ幅
+        self.RATE=4000             #サンプリング周波数
+        self.update_seconds=1      #更新時間[ms]
+        self.audio=pyaudio.PyAudio()
+        self.stream=self.audio.open(format=pyaudio.paInt16,
+                                    channels=1,
+                                    rate=self.RATE,
+                                    input=True,
+                                    frames_per_buffer=self.CHUNK)
+
+        #音声データの格納場所(プロットデータ)
+        self.data=np.zeros(self.CHUNK)
+        self.axis=np.fft.fftfreq(len(self.data), d=1.0/self.RATE)
+
+        #プロット初期設定
+        self.win=pg.GraphicsWindow()
+        self.win.setWindowTitle("SpectrumAnalyzer")
+        self.win.resize(1280,680)
+        self.plt=self.win.addPlot() #プロットのビジュアル関係
+        self.plt.setYRange(0,50)    #y軸の制限
+        self.plt.setXRange(0,self.RATE/2)
+
+        #アップデート時間設定
+        self.timer=QtCore.QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(self.update_seconds)    #10msごとにupdateを呼び出し
+
+    def update(self):
+        self.data=np.append(self.data,self.AudioInput())
+        if len(self.data)/1024 > 10:
+            self.data=self.data[1024:]
+        self.fft_data=self.FFT_AMP(self.data)
+        self.axis=np.fft.fftfreq(len(self.data), d=1.0/self.RATE)
+        self.plt.plot(x=self.axis, y=self.fft_data, clear=True, pen="y")  #symbol="o", symbolPen="y", symbolBrush="b")
+
+    def AudioInput(self):
+        ret=self.stream.read(self.CHUNK)    #音声の読み取り(バイナリ) CHUNKが大きいとここで時間かかる
+        #バイナリ → 数値(int16)に変換
+        #32768.0=2^16で割ってるのは正規化(絶対値を1以下にすること)
+        ret=np.frombuffer(ret, dtype="int16")/32768.0
+        return ret
+
+    def FFT_AMP(self, data):
+        data=np.hamming(len(data))*data
+        data=np.fft.fft(data)
+        data=np.abs(data)
+        return data
+
+if __name__=="__main__":
+    plotwin=PlotWindow()
+    if (sys.flags.interactive!=1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
