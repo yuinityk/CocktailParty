090df88e3a48721e7e2f3a3381797d9c74183ed0
yuinityk <yuuki.initial.yk@gmail.com>
2017-04-12 16:06:13 +0900

    [Fix] rec.pyを少し頑張った
    to be developedのところを変えれば上手く行くような気がする

--- record/rec.py
+++ record/rec.py
@@ -4,8 +4,8 @@ import socket
 import pyaudio
 import wave
 
-WAVLEN = 768044 #This changes according to the RECORD_SECONDS, so you should change here in the future.
-
+WAVLEN = 860160 #This changes according to the RECORD_SECONDS, so you should change here in the future.
+#860160 for recording for 10 seconds
 
 class Recorder():
     def __init__(self,addr,port):
@@ -23,7 +23,7 @@ class Recorder():
         count = p.get_device_count()
         devices = []
         for i in range(count):
-            devices.append(p.get_device_info_by_index())
+            devices.append(p.get_device_info_by_index(i))
         for i, dev in enumerate(devices):
             print (i, dev['name'])
         p.terminate()
@@ -31,7 +31,7 @@ class Recorder():
         FORMAT = pyaudio.paInt16
         CHANNELS = 1               # monoral
         RATE = rate                # sampling rate
-        CHUNK = 2**11              # number of data point
+        CHUNK = 2**9              # number of data point
         RECORD_SECONDS = recsec
         WAVE_OUTPUT_FILENAME = fname
 
@@ -44,7 +44,7 @@ class Recorder():
 
         frames = []
         for i in range(0, int(RATE/CHUNK * RECORD_SECONDS)):
-            data = stream.read(CHUNK)
+            data = stream.read(CHUNK,exception_on_overflow=False)
             frames.append(data)
         print("finished recording")
 
@@ -54,9 +54,10 @@ class Recorder():
 
         waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
         waveFile.setnchannels(CHANNELS)
-        waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+        waveFile.setsampwidth(2)#audio.get_sample_size(FORMAT))
+        #この変更が必要なのかは未検証
         waveFile.setframerate(RATE)
-        waveFile.writeframes(b''.join(frames))
+        waveFile.writeframes(b''.join(frames))#[:WAVLEN])
         waveFile.close()
 
 
@@ -68,6 +69,18 @@ class Client(Recorder):
     '''
     def __init__(self,addr = "localhost",port = 50007):
         super(Client,self).__init__(addr,port)
+
+    def wait(self):
+        self.soc.connect((self.serverIP, self.port))
+        chunks = b''
+        while(1):
+            self.soc.recv(2048)
+            if chunk == "":
+                self.soc.close() #ここでcloseして良いのかは未検証
+                break
+        self.rec()
+        self.send()
+
 
     def send(self,fname = "rec.wav"):
         f = open(fname, "rb")
@@ -92,7 +105,7 @@ class Server(Recorder):
     Send an instruction to record to the client,
     and at the same time record by itself.
     After that receive recorded data from the client
-    and coordinate the two recordings.
+    and Adjust the two recording data.
     '''
     def __init__(self,addr = "localhost",port = 50007):
         super(Server,self).__init__(addr,port)
@@ -114,5 +127,19 @@ class Server(Recorder):
         f.close()
 
     def instruction(self):
+        '''
+        Send an instruction to the client.
+        '''
         pass # to be developed
 
+    def postprocess(self):
+        '''
+        Adjust two recording data.
+        '''
+        pass # to be developed
+
+    def run(self):
+        self.instruction()
+        self.rec()
+        self.receive()
+        self.postprocess()
