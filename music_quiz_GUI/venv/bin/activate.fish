7efb16e7e2314057edb156baf8529e924b62f77f
Masato Odakura <aerospatium@gmail.com>
2017-04-19 18:08:13 +0900

    [Change] venv変更
    全く使い物にならないvenvに無理やりパッケージを
    強制インストール＝グローバル環境のパッケージをコピー&ペースト

--- music_quiz_GUI/venv/bin/activate.fish
+++ music_quiz_GUI/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/odakura/CocktailParty/music_quiz_GUI/venv"
+set -gx VIRTUAL_ENV "/Users/odakura/CocktailParty/visualizer/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
