5a75eeccea6f61a2a2c3ee6bd0cf7cdf69d38497
yuinityk <yuuki.initial.yk@gmail.com>
2017-04-20 19:48:30 +0900

    [Fix] 音声通信出来るようになりました!!!!
    後は時間をずらして調節するだけ

--- record/rec.py
+++ record/rec.py
@@ -7,14 +7,20 @@ import wave
 WAVLEN = 860160 #This changes according to the RECORD_SECONDS, so you should change here in the future.
 #860160 for recording for 10 seconds
 
-class Recorder():
-    def __init__(self,addr,port):
-        self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # enable to access the same port continuously
-        self.serverIP = addr
+serverIP = "10.213.197.163"
+clientIP = "10.213.197.218"
+
+class Recorder(object):
+    def __init__(self,Addr_Server,Addr_Client,port):
+        #self.soc_send    = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #self.soc_receive = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #self.soc_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # enable to access the same port continuously
+        #self.soc_receive.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.serverIP = Addr_Server
+        self.clientIP = Addr_Client
         self.port = port
 
-    def rec(self,rate=44100,recsec=10,fname="rec.wav",idx=0):
+    def rec(self,rate=44100,recsec=3,fname="rec.wav",idx=5):
         '''
         Record with the device of index number idx.
         '''
@@ -67,16 +73,18 @@ class Client(Recorder):
     record,
     and send data to the server.
     '''
-    def __init__(self,addr = "localhost",port = 50007):
-        super(Client,self).__init__(addr,port)
+    def __init__(self,serv = serverIP,cli = clientIP,port = 50007):
+        super(Client,self).__init__(serv,cli,port)
 
     def wait(self):
-        self.soc.connect((self.serverIP, self.port))
+        self.soc_receive = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.soc_receive.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.soc_receive.connect((self.serverIP, self.port))
         chunks = b''
         while(1):
-            self.soc.recv(2048)
-            if chunk == "":
-                self.soc.close() #ここでcloseして良いのかは未検証
+            chunk = self.soc_receive.recv(2048)
+            if chunk == "1":
+                self.soc_receive.close() #ここでcloseして良いのかは未検証
                 break
         self.rec()
         self.send()
@@ -87,9 +95,11 @@ class Client(Recorder):
         data = f.read()
         f.close()
 
-        self.soc.bind((self.addr,self.port))
-        self.soc.listen(1)
-        soc, addr = self.soc.accept()
+        self.soc_send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.soc_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.soc_send.bind((self.clientIP,self.port))
+        self.soc_send.listen(10)
+        soc, addr = self.soc_send.accept()
         print("Connected by" + str(addr))
 
         totalsent = 0
@@ -100,6 +110,7 @@ class Client(Recorder):
         soc.send("") # the sign of the end
         soc.close()
 
+
 class Server(Recorder):
     '''
     Send an instruction to record to the client,
@@ -107,17 +118,19 @@ class Server(Recorder):
     After that receive recorded data from the client
     and Adjust the two recording data.
     '''
-    def __init__(self,addr = "localhost",port = 50007):
-        super(Server,self).__init__(addr,port)
+    def __init__(self,serv=serverIP,cli=clientIP,port = 50007):
+        super(Server,self).__init__(serv,cli,port)
 
     def receive(self):
-        self.soc.connect((self.serverIP, self.port))
+        self.soc_receive = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.soc_receive.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.soc_receive.connect((self.clientIP, self.port))
         chunks = b''
         bytes_recd = 0
         while(1):
-            chunk = self.soc.recv(min(WAVLEN-bytes_recd,2048))
+            chunk = self.soc_receive.recv(min(WAVLEN-bytes_recd,2048))
             if chunk == "":
-                self.soc.close()
+                self.soc_receive.close()
                 break
             chunks += chunk
             bytes_recd += len(chunk)
@@ -126,11 +139,18 @@ class Server(Recorder):
         f.write(chunks)
         f.close()
 
+
     def instruction(self):
-        '''
-        Send an instruction to the client.
-        '''
-        pass # to be developed
+        self.soc_send    = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.soc_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # enable to access the same port continuously
+        self.soc_send.bind((self.serverIP,self.port))
+        self.soc_send.listen(10)
+        soc, addr = self.soc_send.accept()
+        print("Connected by" + str(addr))
+        
+        soc.send("1") # the sign of the end
+        soc.close()
+        self.soc_send.close()
 
     def postprocess(self):
         '''
